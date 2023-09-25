2f5f37972134b9eefb4470645d3124694aa79dde
Aaron Lim <aaronlimchengkiat@gmail.com>
2023-09-26 01:54:01 +0800

    create type safe for job and staff listing

---
+++ types/job-listing.d.ts
@@ -0,0 +1,8 @@
+interface Job {
+  Role_Name: string;
+  Role_Desc: string;
+  Role_ExpiryDate: Date;
+  //   Role_Skill: string[];
+  createdDate: Date;
+  updatedDate: Date;
+}
---
+++ types/staff.d.ts
@@ -0,0 +1,8 @@
+interface Staff {
+  Staff_ID: int;
+  Staff_FName: string;
+  Staff_LName: string;
+  Dept: string;
+  Email: string;
+  Access_Rights: int;
+}
