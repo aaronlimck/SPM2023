d9679f15b1e318c7f110096f1fdd3cfd04e63f01
Aaron Lim <aaronlimchengkiat@gmail.com>
2023-10-02 09:56:22 +0800

    update utils formatDate function

--- lib/utils.ts
+++ lib/utils.ts
@@ -40,7 +40,9 @@ export function isLessThanDayAgo(dateTime: Date) {
   return timeDifference < millisecondsInDay;
 }
 
-export function formatDate(date: Date): string {
+export function formatDate(dateStr: string): string {
+  const date = new Date(dateStr);
+
   const day: string = String(date.getDate()).padStart(2, "0");
   const month: string = String(date.getMonth() + 1).padStart(2, "0");
   const year: string = String(date.getFullYear());
