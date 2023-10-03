fd1ca4fa581fd6083e7bfee012d70000635fa2c1
Aaron Lim <aaronlimchengkiat@gmail.com>
2023-10-03 18:04:18 +0800

    update role management table styling

--- app/(hr)/role-management/roleListingHRTable.tsx
+++ app/(hr)/role-management/roleListingHRTable.tsx
@@ -6,14 +6,12 @@ import { usePathname } from "next/navigation";
 import { useState } from "react";
 
 interface JobListingStaffProps {
-  page: number;
-  search: string | undefined;
+  className?: string;
   jobData: Job[];
 }
 
 export default function JobListingHRTwo({
-  page,
-  search,
+  className,
   jobData,
 }: JobListingStaffProps) {
   const [sortBy, setSortBy] = useState<string | null>(null);
@@ -28,10 +26,10 @@ export default function JobListingHRTwo({
   const sortedJobs = [...jobData].sort((a, b) => {
     if (sortBy === "createdDate" || sortBy === "expiredDate") {
       const dateA = new Date(
-        sortBy === "createdDate" ? a.createdDate : a.Role_ExpiryDate
+        sortBy === "createdDate" ? a.createdAt : a.Role_ExpiryDate
       );
       const dateB = new Date(
-        sortBy === "createdDate" ? b.createdDate : b.Role_ExpiryDate
+        sortBy === "createdDate" ? b.createdAt : b.Role_ExpiryDate
       );
 
       if (sortOrder === "desc") {
@@ -61,25 +59,19 @@ export default function JobListingHRTwo({
   });
 
   return (
-    <>
-      {
-        <p className="text-sm text-gray-500 ml-1 mb-3">
-          {sortedJobs.length} results found
-        </p>
-      }
-
-      <table className="w-full text-sm text-left text-gray-500 mb-6">
+    <div className={className}>
+      <table className={`w-full text-sm text-left text-gray-500 mb-4`}>
         <thead className="text-xs text-gray-700 uppercase bg-accent">
           <tr>
             <th
               scope="col"
-              className="cursor-pointer md:py-3"
+              className="cursor-pointer px-4 py-3"
               onClick={() => {
                 setSortBy("roleName");
                 toggleSortOrder();
               }}
             >
-              <span className="flex md:text-left items-center md:px-2">
+              <span className="flex items-center px-2">
                 Role
                 <ChevronsUpDown className="w-4 h-4 ml-1 text-gray-400" />
               </span>
@@ -87,13 +79,13 @@ export default function JobListingHRTwo({
 
             <th
               scope="col"
-              className="cursor-pointer md:py-3"
+              className="cursor-pointer py-3"
               onClick={() => {
                 setSortBy("createdDate");
                 toggleSortOrder();
               }}
             >
-              <span className="flex justify-center items-center md:pl-3">
+              <span className="flex justify-center items-center pl-3">
                 Created Date
                 <ChevronsUpDown className="w-4 h-4 ml-1 text-gray-400" />
               </span>
@@ -101,95 +93,86 @@ export default function JobListingHRTwo({
 
             <th
               scope="col"
-              className="cursor-pointer md:py-3"
+              className="cursor-pointer py-3"
               onClick={() => {
                 setSortBy("expiredDate");
                 toggleSortOrder();
               }}
             >
-              <span className="flex justify-center items-center md:pl-3">
+              <span className="flex justify-center items-center pl-3">
                 Expired On
                 <ChevronsUpDown className="w-4 h-4 ml-1 text-gray-400" />
               </span>
             </th>
 
-            <th scope="col" className="md:py-3">
-              Status
+            <th scope="col" className="px-6 py-3">
+              <span className="flex flex-row justify-center">Status</span>
             </th>
           </tr>
         </thead>
+
         <tbody>
-          {sortedJobs.map((job, index) => (
-            <tr key={index} className="bg-white border-b">
-              <td
-                scope="row"
-                data-label="Role"
-                className="font-medium text-gray-900 md:text-left md:px-4 whitespace-nowrap"
-              >
-                <Link
-                  className="hover:underline"
-                  href={`${pathname}/${encodeURIComponent(
-                    job.Role_Name.toLowerCase()
-                  )}`}
-                >
-                  {job.Role_Name}
-                </Link>
-              </td>
-
-              <td data-label="Created Date">{formatDate(job.createdDate)}</td>
-
-              <td data-label="Expiry Date">
-                {formatDate(job.Role_ExpiryDate)}
-              </td>
-
-              <td data-label="Status">
-                {isDateInPast(job.Role_ExpiryDate) ? (
-                  <span className="bg-red-100 text-red-800 text-xs font-medium px-2.5 py-0.5 rounded">
-                    Expired
-                  </span>
-                ) : (
-                  <span className="bg-green-100 text-green-800 text-xs font-medium px-2.5 py-0.5 rounded">
-                    Active
-                  </span>
-                )}
-              </td>
+          {sortedJobs.length === 0 ? (
+            <tr className="bg-green-100 w-full">
+              <td className="text-center py-4">No jobs found.</td>
             </tr>
-          ))}
+          ) : (
+            sortedJobs.map((job, index) => (
+              <tr key={index} className={`bg-white border-b`}>
+                <td
+                  scope="row"
+                  className="px-6 py-3 font-medium text-gray-900 whitespace-nowrap"
+                >
+                  <Link
+                    className="hover:underline"
+                    // href={`${pathname}/${encodeURIComponent(
+                    //   job.Role_Listing_Name.toLowerCase()
+                    // )}`}
+                    href={`${pathname}/${job.Role_Listing_ID}`}
+                  >
+                    {job.Role_Listing_Name}
+                  </Link>
+                </td>
+
+                <td className="text-center px-6 py-3 ">
+                  <span className="flex flex-row justify-center">
+                    {formatDate(job.createdAt)}
+                  </span>
+                </td>
+
+                <td className="text-center px-6 py-3">
+                  <span className="flex flex-row justify-center">
+                    {formatDate(job.Role_ExpiryDate)}
+                  </span>
+                </td>
+
+                <td className="px-6 py-3">
+                  <span className="flex flex-row justify-center">
+                    {isDateInPast(job.Role_ExpiryDate) ? (
+                      <span className="bg-red-100 text-red-800 text-xs font-medium px-2.5 py-0.5 rounded">
+                        Expired
+                      </span>
+                    ) : (
+                      <span className="bg-green-100 text-green-800 text-xs font-medium px-2.5 py-0.5 rounded">
+                        Active
+                      </span>
+                    )}
+                  </span>
+                </td>
+              </tr>
+            ))
+          )}
         </tbody>
       </table>
 
-      {/* {sortedJobs.length > 0 && (
-        <div className="w-full flex justify-end mt-4">
-          <div className="border py-1 px-1.5 rounded-xl">
-            <Link
-              href={{
-                pathname: DEFAULT_REDIRECTS.roleManagement,
-                query: {
-                  ...(search ? { search } : {}),
-                  page: page > 1 ? page - 1 : 1,
-                },
-              }}
-              className={`text-sm font-medium py-1 px-2 rounded-md w-fit ${
-                page <= 1 && "pointer-events-none opacity-50"
-              }`}
-            >
-              Previous
-            </Link>
-            <Link
-              href={{
-                pathname: DEFAULT_REDIRECTS.roleManagement,
-                query: {
-                  ...(search ? { search } : {}),
-                  page: page + 1,
-                },
-              }}
-              className={` text-sm font-medium py-1 px-2 rounded-md w-fit `}
-            >
-              Next
-            </Link>
-          </div>
-        </div>
-      )} */}
-    </>
+      {
+        <p className="text-sm text-gray-500 mb-3">
+          Showing{" "}
+          <span className="text-primary font-medium">{sortedJobs.length}</span>{" "}
+          of{" "}
+          <span className="text-primary font-medium">{sortedJobs.length}</span>
+        </p>
+      }
+    </div>
   );
 }
