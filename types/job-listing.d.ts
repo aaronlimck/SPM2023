9b63e295e7251922d1c20cbc6c59a4d9872fd060
Aaron Lim <aaronlimchengkiat@gmail.com>
2023-10-05 16:09:13 +0800

    update job listing type safe

--- types/job-listing.d.ts
+++ types/job-listing.d.ts
@@ -3,7 +3,7 @@ interface Job {
   Role_Listing_Name: string;
   Role_Name: string;
   Role_ExpiryDate: DateTime;
-  Role_Desc?: string;
+  Role_Listing_Desc: string;
   //   Role_Skill: string[];
   createdAt: DateTime;
   updatedAt: DateTime;
