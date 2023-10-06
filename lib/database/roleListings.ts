9680e01df1d9ecc36c8664d45578f4e9bb36e73b
Aaron Lim <aaronlimchengkiat@gmail.com>
2023-10-06 23:23:36 +0800

    update getAllActiveRoleListings function to show latest posts

--- lib/database/roleListings.ts
+++ lib/database/roleListings.ts
@@ -52,7 +52,7 @@ export const getAllActiveRoleListings = async (
         },
       },
       orderBy: {
-        createdAt: "asc",
+        createdAt: "desc",
       },
       include: {
         Role: {
