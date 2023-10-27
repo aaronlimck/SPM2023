c055aefb019d5f3c41b348970ffacf6d288ecfad
ChevyChan <111484440+ChevyChan@users.noreply.github.com>
2023-10-27 21:29:02 +0800

    sort role listing by creation date

--- app/(hr)/role-management/roleListingHRTable.tsx
+++ app/(hr)/role-management/roleListingHRTable.tsx
@@ -7,6 +7,7 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useState } from "react";
 
+
 interface JobListingStaffProps {
   className?: string;
   jobData: Job[];
